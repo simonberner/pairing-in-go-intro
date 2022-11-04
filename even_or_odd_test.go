7855c7dc0a47d372c31a7d23220e8069da70c6b5
Simon Berner <sibebzh@gmail.com>
2022-11-04 12:02:27 +0000

    test: update test-setup

--- even_or_odd_test.go
+++ even_or_odd_test.go
@@ -1,6 +1,13 @@
 package main_test
 
 import (
+	"testing"
     . "github.com/onsi/ginkgo/v2"
     . "github.com/onsi/gomega"
 )
+
+// This is the entry point for Ginkgo - the go test runner will run this function when you run 'go test' or 'ginkgo'.
+func TestEvenOrOdd(t *testing.T) {
+	RegisterFailHandler(Fail) // RegisterFailHandler connects Ginkgo to Gomega.
+	RunSpecs(t, "EvenOrOdd Test Suite") // RunSpecs is the entry point for the Ginkgo spec runner.
+}
